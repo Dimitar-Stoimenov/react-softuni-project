6003c27463c7fcb6d2efcec9760435613ba32bfc
Dimitar Stoimenov <dstoimen0@gmail.com>
2021-12-10 00:57:49 +0200

    remove subscribe btn on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -68,13 +68,7 @@ const Footer = () => {
                             </li>
                         </ul>
                     </div>
-                    <div className="col-auto">
-                        <label className="sr-only" htmlFor="subscribeEmail">Email address</label>
-                        <div className="input-group mb-2">
-                            <input type="text" className="form-control bg-dark border-light" id="subscribeEmail" placeholder="Email address" />
-                            <div className="input-group-text btn-success text-light">Subscribe</div>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
 
