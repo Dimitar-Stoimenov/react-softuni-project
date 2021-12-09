44b1888b8848cb193688e06e322808125ea4df16
Dimitar Stoimenov <dstoimen0@gmail.com>
2021-12-10 00:10:14 +0200

    add NavigationBar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import TopPageInfo from "./components/TopPageInfo";
 import Footer from "./components/Footer";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
 	return (
 		<div id="main">
 			<TopPageInfo />
 
+			<NavigationBar />
+
 			<Footer />
+
 			<script src="/js/jquery-1.11.0.min.js"></script>
 			<script src="/js/jquery-migrate-1.2.1.min.js"></script>
 			<script src="/js/bootstrap.bundle.min.js"></script>
