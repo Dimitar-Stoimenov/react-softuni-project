84e14cbd65103bf093c9ab8920cf225ff8d455b1
Dimitar Stoimenov <dstoimen0@gmail.com>
2021-12-13 02:00:02 +0200

    delete jquery stuff that is unused

--- src/App.js
+++ src/App.js
@@ -36,8 +36,6 @@ function App() {
 			</Routes>
 
 			<Footer />
-			<script src="/js/jquery-1.11.0.min.js"></script>
-			<script src="/js/jquery-migrate-1.2.1.min.js"></script>
 			<script src="/js/bootstrap.bundle.min.js"></script>
 			<script src="/js/templatemo.js"></script>
 			<script src="/js/custom.js"></script>
