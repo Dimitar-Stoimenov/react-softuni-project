582d984d37588d7854501a5a2b2fc8a4b66f3bfb
Dimitar Stoimenov <dstoimen0@gmail.com>
2021-12-18 19:58:39 +0200

    changem message received

--- src/components/Contact/MessageReceived/MessageReceived.js
+++ src/components/Contact/MessageReceived/MessageReceived.js
@@ -3,12 +3,11 @@ const MessageReceived = () => (
     <>
         <div className="container-fluid bg-light py-5">
             <div className="col-md-6 m-auto text-center">
-                <h1 className="h1">We have received your message</h1>
+                <h1 className="h1">We have received your message!</h1>
                 <p>
-
                 </p>
                 <p>
-                    We promise to get back to you as soon as possible!
+                    Our customer support specialists will get back to you as soon as possible.
                 </p>
             </div>
         </div>
