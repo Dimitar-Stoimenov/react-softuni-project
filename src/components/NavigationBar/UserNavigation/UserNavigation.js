8f1e2e790781f6d3b1ca7152b106b337e89cd817
Dimitar Stoimenov <dstoimen0@gmail.com>
2021-12-18 18:10:54 +0200

    add link to cart in user nav

--- src/components/NavigationBar/UserNavigation/UserNavigation.js
+++ src/components/NavigationBar/UserNavigation/UserNavigation.js
@@ -28,7 +28,7 @@ const UserNavigation = () => {
                 </ul>
             </div>
             <div className="navbar align-self-center d-flex">
-                <Link className="nav-icon position-relative text-decoration-none" to="#">
+                <Link className="nav-icon position-relative text-decoration-none" to="/cart">
                     <i className="fa fa-fw fa-cart-arrow-down text-dark mr-1"></i>
                     <span className="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark"></span>
                 </Link>
