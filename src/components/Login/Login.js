b11587776612d6b92fe94bf8800ddca51a24bec5
Dimitar Stoimenov <dstoimen0@gmail.com>
2021-12-18 02:53:07 +0200

    add login component styling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -31,29 +31,20 @@ const Login = () => {
 
     return (
         <div className="container py-5">
-            <div className="row py-5">
+            <div className="col-md-4 col-md-offset-4">
                 <form onSubmit={onLoginHandler} className="col-md-9 m-auto" method="POST" role="form">
                     <div className="row">
-                        <div className="form-group col-md-6 mb-3">
-                            <label for="inputname">Name</label>
-                            <input type="text" className="form-control mt-1" id="name" name="name" placeholder="Name" />
+                        <div class="form-group">
+                            <input class="form-control" autoFocus placeholder="e-mail" name="email" type="text" />
                         </div>
-                        <div className="form-group col-md-6 mb-3">
-                            <label for="inputemail">Email</label>
-                            <input type="email" className="form-control mt-1" id="email" name="email" placeholder="Email" />
+                        <div class="form-group">
+                            <input class="form-control" placeholder="password" name="password" type="password" />
                         </div>
                     </div>
-                    <div className="mb-3">
-                        <label for="inputsubject">Subject</label>
-                        <input type="text" className="form-control mt-1" id="subject" name="subject" placeholder="Subject" />
-                    </div>
-                    <div className="mb-3">
-                        <label for="inputmessage">Message</label>
-                        <textarea className="form-control mt-1" id="message" name="message" placeholder="Message" rows="8"></textarea>
-                    </div>
+
                     <div className="row">
-                        <div className="col text-end mt-2">
-                            <button type="submit" className="btn btn-success btn-lg px-3">Let’s Talk</button>
+                        <div className="col text-start mt-2">
+                            <button type="submit" className="btn btn-success btn-lg px-3">Login</button>
                         </div>
                     </div>
                 </form>
