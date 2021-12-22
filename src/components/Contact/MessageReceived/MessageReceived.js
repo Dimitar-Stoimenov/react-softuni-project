ee3a5338321072444342848b1334f91e885cc27a
Dimitar Stoimenov <dstoimen0@gmail.com>
2021-12-23 00:59:30 +0200

    change styling in Message Received

--- src/components/Contact/MessageReceived/MessageReceived.js
+++ src/components/Contact/MessageReceived/MessageReceived.js
@@ -1,9 +1,10 @@
+import "./MessageReceived.css";
 
 const MessageReceived = () => (
     <>
         <div className="container-fluid bg-light py-5">
             <div className="col-md-6 m-auto text-center">
-                <h1 className="h1">We have received your message!</h1>
+                <h1 className="h1 green-text">We have received your message!</h1>
                 <p>
                 </p>
                 <p>
